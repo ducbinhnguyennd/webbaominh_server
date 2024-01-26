14ea70744325eeffa46fbf31ba88deadd02b87ed
trafdual <traz08102003@gmail.com>
2024-01-26 19:18:40 +0700

    f

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const uri = "mongodb+srv://ducbinhnguyennd:ducbinhnguyennd@cluster0.geuahvt.mongodb.net/baominhshop?retryWrites=true&w=majority";
 mongoose.connect(uri)
         .catch((err)=>{
-            console.log("Loi ket noi CSDL");
+            console.log("Loi ket noi mongo");
             console.log(err);
         });
 module.exports = {mongoose};
